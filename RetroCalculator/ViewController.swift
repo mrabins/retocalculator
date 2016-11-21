6d3d20e69d455ef5cf3dd4f5426aca079b531be9
Mark Rabins <markrabins@gmail.com>
2016-11-21 00:21:16 -0800

    Update constraints, add sound for all button presses

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    var buttonSound: AVAudioPlayer!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do {
+            try buttonSound = AVAudioPlayer(contentsOf: soundURL)
+            buttonSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+    }
+    
+    @IBAction func numberPressed(sender: UIButton) {
+        playSound()
+    }
+    
+    func playSound() {
+        if buttonSound.isPlaying {
+            buttonSound.stop()
+        }
+            buttonSound.play()
     }
 
     override func didReceiveMemoryWarning() {
